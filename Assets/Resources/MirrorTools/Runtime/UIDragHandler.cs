b97f45ac7d1956d4f1230e78b08023cdded95cdb
TUTTIK-FRUTTIK <76999364+TUTTIK-FRUTTIK@users.noreply.github.com>
2026-01-24 22:31:15 +0500

    Moved class in namespace

--- Assets/Resources/MirrorTools/Runtime/UIDragHandler.cs
+++ Assets/Resources/MirrorTools/Runtime/UIDragHandler.cs
@@ -2,54 +2,57 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class UIDragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+namespace MirrorTools
 {
-    public RectTransform dragRectTransform;
-    public Canvas canvas;
-    public bool canBeMoved;
-    public Vector2 lastAnchoredPosition;
+    public class UIDragHandler : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+    {
+        public RectTransform dragRectTransform;
+        public Canvas canvas;
+        public bool canBeMoved;
+        public Vector2 lastAnchoredPosition;
     
-    private Vector2 offset;
-    private bool canDrag;
+        private Vector2 offset;
+        private bool canDrag;
     
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        var results = new List<RaycastResult>();
-        EventSystem.current.RaycastAll(eventData, results);
-        canDrag = results.Count == 1;
-        
-        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                dragRectTransform.parent as RectTransform, 
-                eventData.position, 
-                canvas?.worldCamera, 
-                out Vector2 localCursor))
+        public void OnBeginDrag(PointerEventData eventData)
         {
-            offset = dragRectTransform.anchoredPosition - localCursor;
-        }
-    }
-
-    public void OnDrag(PointerEventData eventData)
-    {
-        if (eventData.button != PointerEventData.InputButton.Left || !canDrag || !canBeMoved)
-            return;
+            var results = new List<RaycastResult>();
+            EventSystem.current.RaycastAll(eventData, results);
+            canDrag = results.Count == 1;
         
-        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                dragRectTransform.parent as RectTransform, 
-                eventData.position, 
-                canvas?.worldCamera, 
-                out Vector2 localCursor))
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                    dragRectTransform.parent as RectTransform, 
+                    eventData.position, 
+                    canvas?.worldCamera, 
+                    out Vector2 localCursor))
+            {
+                offset = dragRectTransform.anchoredPosition - localCursor;
+            }
+        }
+
+        public void OnDrag(PointerEventData eventData)
         {
-            dragRectTransform.anchoredPosition = localCursor + offset;
-            lastAnchoredPosition = dragRectTransform.anchoredPosition;
-        }
-    }
-
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        if (!canBeMoved) return;
+            if (eventData.button != PointerEventData.InputButton.Left || !canDrag || !canBeMoved)
+                return;
         
-        PlayerPrefs.SetFloat("WindowPositionX", dragRectTransform.anchoredPosition.x);
-        PlayerPrefs.SetFloat("WindowPositionY", dragRectTransform.anchoredPosition.y);
-        PlayerPrefs.Save();
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+                    dragRectTransform.parent as RectTransform, 
+                    eventData.position, 
+                    canvas?.worldCamera, 
+                    out Vector2 localCursor))
+            {
+                dragRectTransform.anchoredPosition = localCursor + offset;
+                lastAnchoredPosition = dragRectTransform.anchoredPosition;
+            }
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            if (!canBeMoved) return;
+        
+            PlayerPrefs.SetFloat("WindowPositionX", dragRectTransform.anchoredPosition.x);
+            PlayerPrefs.SetFloat("WindowPositionY", dragRectTransform.anchoredPosition.y);
+            PlayerPrefs.Save();
+        }
     }
 }
